5ba7806d93b99920ac8711bfe0bd2e6e35d34bbe
manavshankar <manavshankar20@gmail.com>
2025-05-12 22:26:12 +0530

    esbug

--- next.config.ts
+++ next.config.ts
@@ -2,23 +2,28 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins:["quality-generally-muskrat.ngrok-free.app"],
+  allowedDevOrigins: ["quality-generally-muskrat.ngrok-free.app"],
+
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ Skip ESLint during production build
+  },
+
   webpack: (config, { isServer, buildId, dev, webpack }) => {
     if (!isServer) {
       config.resolve.fallback = {
         ...config.resolve.fallback,
-        stream: require.resolve('stream-browserify'),
-        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve("stream-browserify"),
+        crypto: require.resolve("crypto-browserify"),
       };
 
       config.plugins.push(
         new webpack.ProvidePlugin({
-          process: 'process/browser',
+          process: "process/browser",
         }),
         new webpack.NormalModuleReplacementPlugin(
           /node:crypto/,
-          (resource : any) => {
-            resource.request = resource.request.replace(/^node:/, '');
+          (resource: any) => {
+            resource.request = resource.request.replace(/^node:/, "");
           }
         )
       );
